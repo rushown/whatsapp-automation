ed0e46257103ff1856456a1c33e1665055d86939
Rocean <roceanadhikari@gmail.com>
2026-05-20 13:59:27 +0530

    updatred vite cingif iwht proper api route

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     port: 3000,
     proxy: {
-      '/api': {
+      '/api/': {
         target: 'http://localhost:5000',
         changeOrigin: true,
       },
